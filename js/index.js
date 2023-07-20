31303ba7c38773b0f86e21de134ed5f6958c937b
Andres Alarcon <valarcon2712@gmail.com>
2023-07-20 17:18:13 -0500

    habilitar ubicacion

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 let ArrayCarrito = JSON.parse(localStorage.getItem("prodCarrito")) || [];
 let totalProductos = ArrayCarrito.length;
 document.getElementsByClassName('conteo')[0].innerHTML=totalProductos;
+
+
+window.addEventListener('load', function (){
+
+    console.log(navigator);
+    console.log(navigator.geolocation);   
+
+})
