990c4c9a04d777544bbbb881ef462fe690fc5ef1
Andres Alarcon <valarcon2712@gmail.com>
2023-07-23 20:07:23 -0500

    orden de datos para mostrar en boton

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const token = 'a8d5faffbdcd4efea4debd47b895bcd1';
+const codTormenta = [200, 201, 202, 230, 231, 232, 233]
 let ArrayCarrito = JSON.parse(localStorage.getItem("prodCarrito")) || [];
 let totalProductos = ArrayCarrito.length;
 document.getElementsByClassName('conteo')[0].innerHTML=totalProductos;
@@ -39,5 +40,15 @@ function renderReco(res){
     document.getElementById("clima").classList.remove("hidden");
     let src = 'https://cdn.weatherbit.io/static/img/icons/'+res.weather.icon+'.png';
     document.getElementById("imgClima").src = src;
+    document.getElementsByClassName('grados')[0].innerHTML=res.app_temp +"°C";
+    document.getElementById('descripcion').innerHTML=res.weather.description;
+    document.getElementById('ubicacion').innerHTML=res.city_name;
+    
+    res.pod=="n" ? document.getElementsByClassName('recomendacion')[0].innerHTML='Recuerda siempre ser visible para autos' : msjReco(res.weather.code);
+
     console.log(res);
+}
+
+function msjReco(code){
+    document.getElementsByClassName('recomendacion')[0].innerHTML='mensaje para dia';
 }
